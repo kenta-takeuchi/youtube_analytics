a7899df6c5d41567ab0796daf6dcec2abee3cd2b
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2021-03-06 12:43:24 +0900

    fix infra code

--- infra/codebuild_cmd/backend/build_docker_image.sh
+++ infra/codebuild_cmd/backend/build_docker_image.sh
@@ -1,16 +1,5 @@
 cd "${CODEBUILD_SRC_DIR}/backend"
-echo Push the docker image
+echo Build started on `date`
 
-echo Logining in to Amazon ECR...
-
-ECR_REPO_URI = "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
-
-aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin $ECR_REPO_URI
-
-docker tag "youtube-movies-analytics-backend:latest" "${ECR_REPO_URI}/youtube-movies-analytics-backend:latest"
-docker push "${ECR_REPO_URI}/youtube-movies-analytics-backend:latest"
-
-echo Push completed on `date`
-
-echo Writing image definitions file...
-printf '[{"name":"youtube-movies-analytics","imageUri":"%s"}]' "${ECR_REPO_URI}/youtube-movies-analytics-backend:latest" > imagedefinitions.json
+docker build -t youtube-movies-analytics-backend:latest ./
+echo Build completed on `date`
